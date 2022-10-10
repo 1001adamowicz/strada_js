48cf1d6325b97dd615f5d1b3e1f84285c50db225
1001adamowicz <97398557+1001adamowicz@users.noreply.github.com>
2022-10-10 19:59:24 +0200

    My first commit Strada

---
+++ main.js
@@ -0,0 +1,121 @@
+/*let usersCounter = 0;
+let newUsers = ++usersCounter;
+console.log(newUsers);
+*/
+
+
+/*let userTrue = true;
+console.log(userTrue);
+console.log(typeof userTrue);
+
+userTrue = String(userTrue);
+console.log(userTrue);
+console.log(typeof userTrue);
+*/
+
+
+/*
+//Строковое преобразование
+let userAge = 58;
+console.log(userAge);
+console.log(typeof userAge);
+
+userAge = String(userAge);
+
+console.log(userAge);
+console.log(typeof userAge);
+*/
+
+
+/*
+//Динамическая типизация
+JavaScript является динамически типизированным.
+То есть тип данных переменной меняется динамически
+в момент присвоения, либо смены её значения. (команда typeof показывает какой
+тип данных)
+А не в момент объявления.
+
+
+let userName; //объявляем переменную
+console.log(typeof userName);
+
+userName = "Фрилансер по жизни";//Строка (String)
+console.log(typeof userName);
+
+userName = 58;//Число (Number)
+console.log(typeof userName);
+
+//кавычки в стоках "" '' ``
+let userAge = 36;
+let userAgeInfo = `Возраст: ${userAge}`;
+console.log(userAgeInfo);
+*/
+
+
+/*if (true) {
+    var size = 15;
+}
+console.log(size);
+*/
+
+/*let userColorEyes = "#CCE5FF";
+let userName = "Вася";
+let user = userName;
+console.log(user);
+console.log(userColorEyes);
+*/
+
+
+/*
+//var (variable) - переменная, появился только в 2015 году дщ let и const
+age = 35;
+console.log(age);
+
+var age;
+*/
+
+
+/*const userProfile = {
+    name: 'Фрилансер',
+    age: 36,
+    massage: 'Живи, а работай в свободное время!'
+}
+console.log(userProfile);
+
+//Далее я меняю значение одного из полей объекта
+userProfile.age = 18;
+console.log(userProfile);
+*/
+
+/*let myLifestyle = 'freelance'; //let - пусть, = оператор присваивания
+let myName = 'iljan';
+let myAge = 34;
+let myMessage = 'Живи, а работай в свободное время!';
+myAge = 23;
+
+
+
+console.log(myLifestyle);
+console.log(myName);
+console.log(myAge);
+*/
+
+
+/*"use strict"
+console.log('Я учу')
+console.log('JS')
+*/
+
+
+/*console.log('Hello world' + ' Учим JS' + ' Это оператор'); //console.log - оператор
+let num = 4; //вырожение присвоения - let это переменная
+console.log(num);
+'Учим' + ' JS'; //вырожение без присвоения
+
+function name () {
+    //Это комментарий
+    console.log('Учим');
+    console.log('JS');
+}
+name();
+*/ 
